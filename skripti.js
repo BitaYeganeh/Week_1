e00ae185566065fe360e9adfada278fd7300f8f0
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-03-31 09:40:29 +0300

    Week1

---
+++ skripti.js
@@ -0,0 +1,17 @@
+console.log("Tämä on Javascript-testi")
+alert("Moi")
+const muuttuja = "Hello"
+function funktio() {
+    let p = 5*6;
+    let x, y, z;
+    (x = 5), (y = 2);
+    z = x + y;
+
+    console.log("This is z: ", z);
+
+    let toinen = "Tänne tulee arvo";
+    console.log(toinen)
+}
+
+ //Kutsu funktio näin;
+ funktio();
